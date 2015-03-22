e4a3074f91ca23461f4c53c3e8567f31aa8a17a6
ShiNSanity75 <jj06280705@gmail.com>
2015-03-22 16:21:57 +0800

    Update Account.java

--- Account.java
+++ Account.java
@@ -6,4 +6,9 @@ pubilc class Account {
 	private double annualInterestRate; //Store the current interest rate
 	private Date dateCreated; //stores the date when the account was created
 	
+	public Account() {
+		id = 0;
+		balance = 0;
+		annualInterestRate = 0;
+	}
 }
