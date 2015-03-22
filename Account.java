806596878929a6c2b0362b6062ab96f18c7efd7a
ShiNSanity75 <jj06280705@gmail.com>
2015-03-22 16:22:35 +0800

    完成constructor

--- Account.java
+++ Account.java
@@ -6,9 +6,16 @@ pubilc class Account {
 	private double annualInterestRate; //Store the current interest rate
 	private Date dateCreated; //stores the date when the account was created
 	
+	//no-arg constructor
 	public Account() {
 		id = 0;
 		balance = 0;
 		annualInterestRate = 0;
+	}
+	
+	//constructor with the specified id and initial balance
+	public Account(int newID, double newBalance) {
+		id = newID;
+		balance = newBalance;
 	}
 }
