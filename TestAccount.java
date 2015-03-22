f32d358c7399bf5f13bd5ffbd9b2a52e66753d41
ShiNSanity75 <jj06280705@gmail.com>
2015-03-22 23:17:33 +0800

    print 所有結果的test

---
+++ TestAccount.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+public class TestAccount {
+  //main method
+  public static void main(String[] args) {
+  	Account account = new Account(1206, 20000, 0.045);
+	  Account savingAccount = new SavingAccount(1023, 20000, 0.045);
+	  Account CheckingAccount = new CheckingAccount(1895, 20000, 0.045);
+		
+	  //display the result
+	  System.out.println("account\n" + account.toString() + "\n");
+		System.out.println("savingAccount\n" + savingAccount.toString() + "\n");
+		System.out.println("CheckingAccount\n" + CheckingAccount.toString() + "\n");
+	}
+}
