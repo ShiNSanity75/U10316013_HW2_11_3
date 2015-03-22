7ff62669f3e281968f2dda3c5ecfca3e2abeb265
ShiNSanity75 <jj06280705@gmail.com>
2015-03-22 16:00:49 +0800

    先把變數宣告完成

---
+++ Account.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+pubilc class Account {
+	private int id = 0; //ID for the account
+	private double balance = 0; //Balance for the account
+	private double annualInterestRate = 0; //Store the current interest rate
+	private Date dateCreated; //stores the date when the account was created
+	
+}
